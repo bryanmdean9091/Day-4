5727749a5df2de48696412c600de8a6d4157d46c
bryanmdean9091 <bryanmdean9091@gmail.com>
2022-08-17 21:14:01 -0700

    first commit

---
+++ java.js
@@ -0,0 +1,18 @@
+let numOne = parseInt(prompt("Please enter a number", 10));
+let numTwo = parseInt(prompt("Please enter another number", 10));
+if (numOne === numTwo) {
+    alert("These numbers are identical");
+}
+else if (numOne !=  Number) {
+    alert(`${numOne} is not a number`);
+}
+else if (numOne > numTwo) {
+    alert(`The larger of these two numbers is ${numOne}`);  
+}
+else alert(`The larger of these two numbers is ${numTwo}`);
+
+
+
+
+
+
