414339e7fb3a678a94017c80817915ee25eacf4e
bryanmdean9091 <bryanmdean9091@gmail.com>
2022-08-22 18:03:35 -0700

    take-home

--- java.js
+++ java.js
@@ -1,19 +1,21 @@
+// function funcCompare() {
+//     let numOne = parseInt(prompt("Please enter a number", 10));
 
-// let numOne = parseInt(prompt("Please enter a number", 10));
-// let numTwo = parseInt(prompt("Please enter a second number", 10));
+//     let numTwo = parseInt(prompt("Please enter a second number", 10));
+//     console.log(typeof numOne)
+//     if (!isNaN(numOne) && !isNaN(numTwo)) {
+//         if (numOne === numTwo) {
+//             alert("These numbers are identical");
+//         } else if (numOne > numTwo) {
+//             alert(`The larger of these two numbers is ${numOne}`);
+//         } else {
+//             alert(`The larger of these two numbers is ${numTwo}`);
+//         }
+//     } else if (isNaN(numOne) || isNaN(numTwo)) {
+//         alert("Please enter a valid number");
+//         funcCompare()
 
-//  if (numOne === numTwo) {
-//     alert("These numbers are identical");
+//     }
+
 // }
-
-// else if (numOne > numTwo) {
-//     alert(`The larger of these two numbers is ${numOne}`);  
-// }
-// else  {
-//     alert(`The larger of these two numbers is ${numTwo}`);
-// }
-
-
-
-
-
+// funcCompare()
--- review.js
+++ review.js
@@ -23,22 +23,31 @@ switch (new Date().getDay()) {
 }
 console.log(day);
 
+
+const img = document.createElement("img");
+img.src = "https://www.google.com/imgres?imgurl=https%3A%2F%2Fimages.pexels.com%2Fphotos%2F1563356%2Fpexels-photo-1563356.jpeg%3Fcs%3Dsrgb%26dl%3Dpexels-craig-adderley-1563356.jpg%26fm%3Djpg&imgrefurl=https%3A%2F%2Fwww.pexels.com%2Fsearch%2Ffree%2F&tbnid=9KN8wQ21Z2njvM&vet=12ahUKEwiYhcCV_dj5AhWikWoFHSZ8ASEQMygAegUIARDbAQ..i&docid=pB2ggFAh5BKxrM&w=5472&h=3648&q=pexels&ved=2ahUKEwiYhcCV_dj5AhWikWoFHSZ8ASEQMygAegUIARDbAQ"
+
 var bandMember = prompt("Enter the first name of a member of The Beatles");
 switch(bandMember) {
 case "John", "john":
-    alert("John played rhythm guitar");
+    alert("John played rhythm guitar")
+    body.appendChild(img);
     break;
     case "Ringo", "ringo":
         alert("Ringo played the drums");
+        document.querySelector("#a2")
         break;
         case "Paul","paul":
             alert("Paul played the bass");
+            document.querySelector("#a3")
             break;
             case "George", "george":
             alert("George played lead guitar");
+            document.querySelector("#a4")
             break;
             case (""):
             default:
                 alert("This is not a member of The Beatles");
+                document.querySelector("#a5")
           
 }
